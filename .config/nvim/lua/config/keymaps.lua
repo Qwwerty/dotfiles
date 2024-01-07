b5727193028746605c781f5168674caf4b4b4e22
Rhalf Oliveira <rhalfsouzaa@gmail.com>
2024-01-07 18:23:48 -0300

    feat: rest-nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -36,3 +36,6 @@ keymap.set("n", "<C-w><down>", "<C-w>-")
 keymap.set("n", "<C-j>", function()
   vim.diagnostic.goto_next()
 end, opts)
+
+-- Client rest
+keymap.set("n", "<leader>rn", require("rest-nvim").run, opts)
